7d4b540b0b1dfd91a66e8e46c1191e207e9a0382
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-10-22 18:22:49 -0400

    started project; added public and view directories

--- lib/etsiest.rb
+++ lib/etsiest.rb
@@ -1,5 +1,29 @@
 require "etsiest/version"
+require "etsy"
+require "pry"
+require "sinatra/base"
+Etsy.api_key = ENV['ETSY_KEY'] 
 
 module Etsiest
-  # Your code goes here...
+  class App < Sinatra::Base
+    set :logging, true
+
+
+  get "/search" do    # generates data used to duplicate page; drill down and store it! Send data to erb and then drill down? 
+  response = Etsy::Request.get('/listings/active', :includes => ['Images', 'Shop'], :keywords => 'whiskey') 
+  response.result
+  binding.pry
+  erb :index, locals: {results: response}
+  end
+
+#binding.pry
+
+
+
+    run! if app_file == $0
+  end
 end
+
+#
+# ETSY_KEY=foobarbaz bundle exec ruby lib/etsiest.rb
+# alias etsy='ETSY_KEY=ENTER KEY HERE bundle exec ruby lib/etsiest.rb'
